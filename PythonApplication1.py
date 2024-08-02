200ba78f460347eb68dc06e4521d761ce002dcb6
哲瀚 林 <1294092988@qq.com>
2024-08-02 14:14:42 +0800

    有错误

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,20 @@
+﻿
+import os,sys,time
+import random
+filepath = 'save-lzh.xml'
+#filepath = sys.argv[1]
 
+
+def extract_string(input_str,name,name2):
+    if name in input_str:
+        return input_str[input_str.index(name) + len(name) : input_str.index(name2)]
+    else:
+        return """error 0x1
+    no_str_in_inport
+    """
+
+if __name__ == "__main__":
+    file = open(filepath,'r')
+    filedata = file.readline()
+    result = extract_string(filedata,'7','p')
+    print(result)
