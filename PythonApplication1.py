9549c9ebe6a7ba65fcebd046d938df0ea39e79d2
哲瀚 林 <1294092988@qq.com>
2024-08-02 22:35:11 +0800

    开发版 可以输出所有节点名称和IP

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,20 +1,43 @@
 ﻿
 import os,sys,time
+import re
 import random
+
 filepath = 'save-lzh.xml'
 #filepath = sys.argv[1]
 
 
-def extract_string(input_str,name,name2):
+def extract_string(input_str,name):
     if name in input_str:
-        return input_str[input_str.index(name) + len(name) : input_str.index(name2)]
+        return input_str[input_str.index(name) + 1 : input_str.index(name) + 75]
     else:
-        return """error 0x1
-    no_str_in_inport
-    """
+        return 0
+    
+def find_string(input_str, name):
+    lzh = str(input_str) 
+    for i in range(len(lzh)):
+        if lzh[i:].startswith(name):
+            return lzh[i + len(name) :]
+    return ""
 
 if __name__ == "__main__":
-    file = open(filepath,'r')
-    filedata = file.readline()
-    result = extract_string(filedata,'7','p')
-    print(result)
+    file = open(filepath,'r',encoding='utf-8')
+    line = 1
+    while True:
+        line = line + 1
+        filedata = file.readline(line)
+        result = extract_string(filedata,'<computer name=')
+        opt = find_string(result,"ter")
+        #print(result)
+        if result != 0:
+            print(opt)
+
+            
+                    
+      
+        #
+        #print(opt)
+        #
+    
+    
+
